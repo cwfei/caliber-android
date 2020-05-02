5186a510585b42a8635d4d963ba899c0d581b58a
cw fei <cwfei@snappymob.com>
2020-05-02 16:29:30 +0800

    Fix incorrect watch service import

--- app/src/main/java/com/caliber/app/ui/watch/editor/WatchEditorViewModel.kt
+++ app/src/main/java/com/caliber/app/ui/watch/editor/WatchEditorViewModel.kt
@@ -1,9 +1,11 @@
 package com.caliber.app.ui.watch.editor
 
 import androidx.lifecycle.ViewModel
-import java.nio.file.WatchService
+import com.caliber.app.service.WatchService
 import javax.inject.Inject
 
-class WatchEditorViewModel @Inject constructor(private val watchService: WatchService) : ViewModel() {
+class WatchEditorViewModel @Inject constructor(
+    private val watchService: WatchService
+) : ViewModel() {
 
 }
