dd0ec7e97ed664050a3faca50421ca4ba5b9b72f
cw fei <cwfei@snappymob.com>
2020-05-04 01:05:01 +0800

    Initial true time (NTP) implementation

--- app/src/main/java/com/caliber/app/App.kt
+++ app/src/main/java/com/caliber/app/App.kt
@@ -1,10 +1,13 @@
 package com.caliber.app
 
 import android.app.Application
+import android.util.Log
 import com.caliber.app.di.AppInjector
+import com.instacart.library.truetime.TrueTimeRx
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class App : Application(), HasAndroidInjector {
@@ -16,6 +19,12 @@ class App : Application(), HasAndroidInjector {
         super.onCreate()
 
         AppInjector.init(this)
+
+        TrueTimeRx.build()
+            .withSharedPreferencesCache(this)
+            .initializeRx("time.apple.com")
+            .subscribeOn(Schedulers.io())
+            .subscribe()
     }
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
--- app/src/main/java/com/caliber/app/ui/clock/ClockFragment.kt
+++ app/src/main/java/com/caliber/app/ui/clock/ClockFragment.kt
@@ -1,15 +1,28 @@
 package com.caliber.app.ui.clock
 
 import android.os.Bundle
+import android.text.format.DateUtils
+import android.text.format.DateUtils.SECOND_IN_MILLIS
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 
 import com.caliber.app.R
+import com.instacart.library.truetime.TrueTimeRx
+import kotlinx.android.synthetic.main.fragment_clock.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class ClockFragment : Fragment() {
+
+    private val formatter = SimpleDateFormat.getTimeInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +34,34 @@ class ClockFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         titleTextView.text = getString(R.string.atomic_clock)
+
+        lifecycleScope.launch {
+            while (true) {
+                delay(
+                    1000 - TrueTimeRx.now()
+                        .time
+                        .toString()
+                        .takeLast(3)
+                        .toLong()
+                )
+
+                updateViews()
+            }
+        }
+
+        updateViews()
+    }
+
+    private fun updateViews() {
+        if (!TrueTimeRx.isInitialized()) {
+            return
+        }
+
+        val deviceTime = Calendar.getInstance().time
+        val trueTime = TrueTimeRx.now()
+        val millisDifference = trueTime.time - deviceTime.time
+
+        dateTextView.text = formatter.format(trueTime)
+        timeDifferentTextView.text = "${millisDifference} ms behind system clock"
     }
 }
